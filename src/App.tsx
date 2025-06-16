9c74baab012c7e34b1375f152b6987fb5c7f9850
nianod <arnoldkk422@gmail.com>
2025-06-16 15:52:44 +0300

    fav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 
 const App = () => {
 
-  const 
+  const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <h1>you clicked {count} times</h1>
+      <button onClick={() => setCount(count + 500000)}>increment</button>
     </div>
   )
 }
