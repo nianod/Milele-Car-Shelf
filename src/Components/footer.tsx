7e896980694d94f5ad1193a726eda3acc8ee4c08
nianod <arnoldkk422@gmail.com>
2025-06-17 14:00:36 +0300

    footer

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -1,35 +1,50 @@
+import { FaInstagram, FaTwitter, FaFacebook, FaYoutube, FaLinkedin } from "react-icons/fa"
 
 const Footer = () => {
-    const footerContents = [
-        {label: "About Us", href: "#"},
-        {label: "Contact Us", href: "#"},
-        {label: "FAQ's", href: "#"},
-        {label: "Privacy Policy", href: "#"},
-        {label: "Career", href: "#"}
-    ]
-    const brands = [
-        {label: "Maserati"},
-        {label: "Mercedez-GLM"},
-        {label: "Ferrari"},
-        {label: "QX-80"},
-        {label: "Range-Rover-2025"},
-        {label: "Mc-Laren"}
-    ]
+  const footerContents = [
+    { label: "About Us", href: "#" },
+    { label: "Contact Us", href: "#" },
+    { label: "FAQ's", href: "#" },
+    { label: "Privacy Policy", href: "#" },
+    { label: "Career", href: "#" },
+  ]
+  const brands = [
+    { label: "Maserati" },
+    { label: "Mercedez-GLM" },
+    { label: "Ferrari" },
+    { label: "QX-80" },
+    { label: "Range-Rover-2025" },
+    { label: "Mc-Laren" },
+  ]
+  const SocialMeadia = [
+    { Instagram: "Maserati" },
+    { Twitter: "Mercedez-GLM" },
+    { Facebook: "Ferrari" },
+    { LinkedIn: "QX-80" },
+    { Youtube: "Range-Rover-2025" },
+  ]
   return (
-    <div>
-      <div>
-        <p>Useful links</p>
-        {footerContents.map((item, index) => 
-        <a key={index} href={item.href}>{item.label}</a>
-        )}
+    <div className="flex justify-between p-3 bg-blue-950 text-gray-400">
+      <div className="gap-5">
+        <p className="mb-2 font-bold text-xl">Useful links</p>
+        {footerContents.map((item, index) => (
+          <a key={index} href={item.href}
+           className="flex flex-col"
+          >
+            {item.label}
+          </a>
+        ))}
       </div>
-      <div>
-        {brands.map((item, index) => 
-         <p key={index}>{item.label}|</p>
-        )}
+      <div className="max-w-md">
+        <p className="font-bold text-xl mb-2">Top Brands</p>
+        <div className="flex flex-wrap gap-2">
+            {brands.map((item, index) => (
+              <p key={index}>{item.label} |</p>
+            ))}        
+        </div>
       </div>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
