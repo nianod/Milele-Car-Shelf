1b989c89a7d6ef3d28922b269cfb4f99ba95bea7
nianod <arnoldkk422@gmail.com>
2025-06-17 13:37:33 +0300

    footer

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -7,12 +7,25 @@ const Footer = () => {
         {label: "Privacy Policy", href: "#"},
         {label: "Career", href: "#"}
     ]
+    const brands = [
+        {label: "Maserati"},
+        {label: "Mercedez-GLM"},
+        {label: "Ferrari"},
+        {label: "QX-80"},
+        {label: "Range-Rover-2025"},
+        {label: "Mc-Laren"}
+    ]
   return (
     <div>
       <div>
         <p>Useful links</p>
         {footerContents.map((item, index) => 
         <a key={index} href={item.href}>{item.label}</a>
+        )}
+      </div>
+      <div>
+        {brands.map((item, index) => 
+         <p key={index}>{item.label}|</p>
         )}
       </div>
     </div>
